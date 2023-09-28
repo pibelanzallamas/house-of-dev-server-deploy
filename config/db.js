202c7ad9a2b0ed3ba0ec5d1e213d8298d92c4c2b
Brandon Miguel Castillo Llajamango <pibelanzallamas@MacBook-Pro-de-Brandon.local>
2023-09-28 14:57:41 -0300

    deploy fixes

--- config/db.js
+++ config/db.js
@@ -1,11 +1,16 @@
 require("dotenv").config();
 const Sequelize = require("sequelize");
 
-const db = new Sequelize("housedb", "brandon", process.env.DB_PASSWORD, {
-  host: process.env.DB_HOST,
-  dialect: "postgres",
-  logging: false,
-  timezone: "-03:00",
-});
+const db = new Sequelize(
+  "housedb",
+  "brandon",
+  "oYJOZG0sw10oTC21HOgICcDa3mh6JhJu",
+  {
+    host: "dpg-ck4fqqk2kpls73e3ovdg-a",
+    dialect: "postgres",
+    logging: false,
+    timezone: "-03:00",
+  }
+);
 
 module.exports = db;
