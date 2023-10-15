67a8938322401f6b77d268144d6f6d872362bcc3
Brandon Miguel Castillo Llajamango <pibelanzallamas@MacBook-Pro-de-Brandon.local>
2023-10-15 09:54:13 -0300

    fixes

--- config/db.js
+++ config/db.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 const Sequelize = require("sequelize");
+
 const db = new Sequelize(
   "housedb",
   "brandon",
--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const db = require("./config/db");
 const routes = require("./routes");
 const models = require("./models");
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
+
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api", routes);
@@ -13,14 +21,6 @@ app.use("/api", routes);
 app.use("/api", (req, res) => {
   res.sendStatus(404);
 });
-
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
-    credentials: true,
-  })
-);
 
 app.use((err, req, res, next) => {
   console.log("ERROR");
